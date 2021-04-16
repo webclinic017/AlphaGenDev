540427b367afb041b6667779ecc9e2c7d703d780
Juan Imbett <jfimbett@gmail.com>
2021-04-16 22:08:58 +0200

    Progress in computing the signal

--- api/FinalData.py
+++ api/FinalData.py
@@ -37,42 +37,173 @@ from bs4 import BeautifulSoup
 import os
 import time
 from multiprocessing import Process, Queue
+import matplotlib.pyplot as plt
 
-
-
-# %%
-# We start with the pricing data and then add the accouting data
+#%% Loads basic files
 PATH_TO_SEC_DATA=os.environ['PATH_TO_SEC_DATA']
 PATH_TO_COMPUSTAT_CRSP_DATA=os.environ['PATH_TO_COMPUSTAT_CRSP_DATA']
 
-df_yahoo=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, 'yahoo_finance', 'yahoo_finance1.csv'))
-df_yahoo=df_yahoo.rename(columns={'date' : 't_day'})
-df_yahoo=df_yahoo.rename(columns={'tic' : 'ticker'})
-df_yahoo.ticker=df_yahoo.ticker.apply(lambda x : x.lower())
-df_sec=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, "fundamentals.csv"))
+#%% Appends yahoo finance data
+# To avoid unnecessary data loading,
+def append_yahoo_finance(tickers, id):
+    
+    df_yahoo=pd.DataFrame()
 
-df_link=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, "cik_ticker.csv"))
-# %%
+    for ticker in tqdm(tickers):
+        path_file=os.path.join(PATH_TO_SEC_DATA, 'yahoo_finance','data', f'{ticker}.csv')
+        if os.path.exists(path_file):
+            temp=pd.read_csv(path_file)
+            df_yahoo=df_yahoo.append(temp)
 
-# Merges and drop _merge==2, then we carry forward the observation
-
-# First we need the cik, for this one has to be an inner join, all of them should appear
-
-df=df_yahoo.merge(df_link, on=['ticker'])
-df=df.merge(df_sec, how='left',on=['cik', 't_day'])
-
-#%% Forward filling of the variables
-
-df=df.sort_values(['cik', 't_day'])
-
-to_ffill=['atq', 'cheq', 'cshoq', 'oiadpq', 'seqq']
-
-for var in to_ffill:
-    df[var] = df.groupby(['cik'])[var].ffill()
+    df_yahoo=df_yahoo.rename(columns={'date' : 't_day'})
+    df_yahoo=df_yahoo.rename(columns={'tic' : 'ticker'})
+    def std_ticker(x):
+        try:
+            return x.lower()
+        except:
+            return x
+    df_yahoo.ticker=df_yahoo.ticker.apply(std_ticker)
+    #return df_yahoo
+    df_yahoo.to_csv(os.path.join(PATH_TO_SEC_DATA, 'yahoo_finance', f"aggregated_yf{id}.csv"), index=False)
 
 # %%
- # Some checks on that data
 
- tic_unique=set(df.ticker)
- tic_uniqueyf=set(df_yahoo.ticker)
+def merge_yf_sec():
+# We start with the pricing data and then add the accouting data
+    df_yahoo=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, 'yahoo_finance', 'aggregated_yf.csv'))
+
+    df_sec=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, "fundamentals.csv"))
+    df_sec=df_sec.sort_values(['cik', 't_day'])
+    df_link=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, "cik_ticker.csv"))
+    df=df_yahoo.merge(df_link, on=['ticker']) #31414117
+    df=df.dropna() #31089236
+    df=df.sort_values(['cik', 't_day'])
+
+    df=df.merge(df_sec, how='left',on=['cik', 't_day'])
+    to_ffill=['atq', 'cheq', 'cshoq', 'oiadpq', 'seqq']
+
+    for var in to_ffill:
+        df[var] = df.groupby(['cik'])[var].ffill()
+
+    # we need the sec or compustat data
+    
+
+    etfs=["IYW", "IXC", "IYH", "IDU", "IJU", "IAU", "QUAL", "EFG", "^GSPC"]
+
+    before = int(time.mktime(datetime.date(1990,1,1).timetuple()))# Some starting date so I dont download all
+    today=int(time.time())
+
+    for ticker in etfs:
+        url=f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={before}&period2={today}&interval=1d&events=history&includeAdjustedClose=true"
+    
+        temp_df=pd.read_csv(url)
+        temp_df.loc[1:, (f'{ticker}_ret')]=temp_df['Adj Close'][1:].values/temp_df['Adj Close'][:-1].values-1.0
+        temp_df=temp_df.rename(columns={"Date": "date", "Open": "open", "High" : "high",
+                                            "Low" : "low", "Close" : "close", "Adj Close" : "adjclose",
+                                            "Volume" : "volume"})
+        temp_df=temp_df.rename(columns={'date' : 't_day'})
+        temp_df=temp_df.filter(['t_day', f'{ticker}_ret'])
+        
+        temp_df.to_csv(os.path.join(PATH_TO_SEC_DATA, "yahoo_finance", "data_etfs", f"{ticker}.csv"), index=False)
+        df=df.merge(temp_df, how='left',on=['t_day'])
+    
+    # use asreg from stata for the beta
+    df=df.sort_values(['cik', 't_day'])
+
+    covariances=df.groupby(['cik'])[['ret', '^GSPC_ret']].rolling(252, min_periods=0).cov().unstack(2)
+    
+    icov=covariances['^GSPC_ret'].reset_index()
+    df['beta']=icov['ret']/icov['^GSPC_ret']
+
+    df['me']=df['adjclose']*df['cshoq']
+
+    df['avg_size']=df.groupby(['cik'])[['me']].rolling(30, min_periods=0).mean().reset_index()['me'] #.unstack(2)
+    #computes 30 days average
+    df['bm']=df['seqq']/df['avg_size']
+    df['me']=np.log(df['me'])
+
+    df['avg_ret']=df.groupby(['cik'])[['ret']].rolling(30, min_periods=0).mean().reset_index()['ret'] #.unstack(2)
+   
+    df['prof']=df['oiadpq']/df['atq']
+  
+    df['cash']=df['cheq']/df['atq']
+
+    # mret?
+    df['prc-21']=df.groupby(['cik']).adjclose.shift(21)
+    df['prc+21']=df.groupby(['cik']).adjclose.shift(-21)       
+    df['mret']=df['adjclose']/df['prc-21']-1.0
+    df['Fret']=df['prc+21']/df['adjclose']-1.0
+    # Stability of the data
+    temp=df[df.cik==320193]
+
+    plt.plot(temp.t_day, temp.ret)
+
+    tic_unique=set(df.ticker)
+    #%%
+
+    # Compute market beta
+
+    # Some analysis of the database?
+    print(f"Memory Usage: {df.memory_usage()}")
+    print(f"Number of rows: {len(df)}")
+    print(f"Sample: {min(df.t_day)} - {max(df.t_day)}")
+    print(f"Companies: {len(tic_unique)}")
+    #%%
+    # Persistence, csv seems still like the best alternative
+
+    # build variables here
+
+    # Save them year after year, we might use maximum data from 2 years for normal signal computations
+    df['year']=df.t_day.apply(lambda x: int(x[:4]))
+    for y in tqdm(range(min(df.year), max(df.year))):
+        temp=df[df.year==y]
+        temp.to_csv(os.path.join(PATH_TO_SEC_DATA, f'information_set{y}.csv' ), index=False)
 # %%
+def aggregate_yf_csv():
+
+    df=pd.DataFrame()
+    for i in tqdm(range(os.cpu_count())):
+        temp=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, 'yahoo_finance', f"aggregated_yf{i}.csv"))
+        # Keep only the relevant variables
+        keep_var=['t_day','open','high','low','close','adjclose','volume','ticker',	'ret']
+        temp=temp.filter(keep_var)
+        df=df.append(temp)
+
+    df.to_csv(os.path.join(PATH_TO_SEC_DATA, 'yahoo_finance', f"aggregated_yf.csv"), index=False)
+
+
+if __name__=='__main__':
+    start=time.time()
+    merge_yf_sec()
+    end=time.time()
+    time_ellapsed=end-start
+    t=round(time_ellapsed, 2)
+    print(f"{t} seconds")
+    
+
+
+
+
+# if __name__=='__main__':
+
+#     start=time.time()
+#     df=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, "cik_ticker.csv"))
+#     tickers=df.ticker
+#     nb=os.cpu_count() 
+#     batches=np.array_split(tickers,nb)
+#     print(f"Batches of size {[len(batch) for batch in batches]}")
+
+#     print(f"Spliting the sample into {len(batches)} batches")
+#     processes = [Process(target=append_yahoo_finance, args=(batches[i], i)) for i in [2]] #range(nb)]
+
+#     for p in processes:
+#         p.start()
+
+#     for p in processes:
+#         p.join()
+
+#     end=time.time()
+#     time_ellapsed=end-start
+#     t=round(time_ellapsed, 2)
+#     n=len(tickers)
+#     print(f"{t} seconds - for {n} tickers")
--- api/YahooFinanceData.py
+++ api/YahooFinanceData.py
@@ -25,6 +25,7 @@
 # 	
 #--------------------p=E[mx]------------------------------
 
+#%%
 import pandas as pd 
 import numpy as np
 import datetime
@@ -37,9 +38,9 @@ import os
 import time
 from multiprocessing import Process, Queue
 
+#%%
 
-
-def get_yahoo_finance_data(t, id):
+def get_yahoo_finance_data(t):
     PATH_TO_SEC_DATA=os.environ['PATH_TO_SEC_DATA']
     PATH_TO_COMPUSTAT_CRSP_DATA=os.environ['PATH_TO_COMPUSTAT_CRSP_DATA']
     t=tqdm(t)
@@ -53,35 +54,51 @@ def get_yahoo_finance_data(t, id):
     for ticker in t:
         url=f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={before}&period2={today}&interval=1d&events=history&includeAdjustedClose=true"
         try:
-            time.sleep(0.5)
-            temp_df=pd.read_csv(url)
-            temp_df['tic']=ticker.upper()
-            temp_df['ret']=[np.nan for i in temp_df['Adj Close']]
+            if not os.path.exists(os.path.join(PATH_TO_SEC_DATA, "yahoo_finance", "data", f"{ticker}.csv")):
+                time.sleep(1)
+                t.set_description(f"Downloading {ticker} - {not_found}  not found ")
+                t.refresh() # to show immediately the update
+                temp_df=pd.read_csv(url)
+                temp_df['tic']=ticker.upper()
+                temp_df['ret']=[np.nan for i in temp_df['Adj Close']]
+                try:
+                    temp_df.loc[1:, ('ret')]=temp_df['Adj Close'][1:].values/temp_df['Adj Close'][:-1].values-1.0
+                except:
+                    temp_df.loc[1:, ('ret')]=np.nan
+                temp_df['year']=[datetime.datetime.strptime(date, '%Y-%m-%d').year for date in temp_df.Date]
+                temp_df['month']=[datetime.datetime.strptime(date, '%Y-%m-%d').month for date in temp_df.Date]
+                temp_df['day']=[datetime.datetime.strptime(date, '%Y-%m-%d').day for date in temp_df.Date]
 
-            temp_df.loc[1:, ('ret')]=temp_df['Adj Close'][1:].values/temp_df['Adj Close'][:-1].values-1.0
-
-            temp_df['year']=[datetime.datetime.strptime(date, '%Y-%m-%d').year for date in temp_df.Date]
-            temp_df['month']=[datetime.datetime.strptime(date, '%Y-%m-%d').month for date in temp_df.Date]
-            temp_df['day']=[datetime.datetime.strptime(date, '%Y-%m-%d').day for date in temp_df.Date]
-
-            # I want to make sure column names are 
-            # date	open	high	low	close	adjclose	volume	tic	t_day	t_month	ret
-            temp_df=temp_df.rename(columns={"Date": "date", "Open": "open", "High" : "high",
-                                    "Low" : "low", "Close" : "close", "Adj Close" : "adjclose",
-                                    "Volume" : "volume"})
+                # I want to make sure column names are 
+                # date	open	high	low	close	adjclose	volume	tic	t_day	t_month	ret
+                temp_df=temp_df.rename(columns={"Date": "date", "Open": "open", "High" : "high",
+                                        "Low" : "low", "Close" : "close", "Adj Close" : "adjclose",
+                                        "Volume" : "volume"})
 
 
-            yahoo_finance=yahoo_finance.append(temp_df)
-            
+                #yahoo_finance=yahoo_finance.append(temp_df)
+                temp_df.to_csv(os.path.join(PATH_TO_SEC_DATA, "yahoo_finance", "data", f"{ticker}.csv"), index=False)
+                t.set_description(f"{ticker} Downloaded - {not_found}  not found ")
+                t.refresh()
+            else:
+                t.set_description(f"{ticker}.csv already exists- {not_found}  not found ")
+                t.refresh() # to show immediately the update
+
         except: 
             not_found+=1
             t.set_description(f"{ticker} not processed - {not_found} in total")
             t.refresh() # to show immediately the update
             continue
 
-    yahoo_finance.to_csv(os.path.join(PATH_TO_SEC_DATA, "yahoo_finance",f"yahoo_finance{id}.csv"), index=False)
+    #yahoo_finance.to_csv(os.path.join(PATH_TO_SEC_DATA, "yahoo_finance",f"yahoo_finance{id}.csv"), index=False)
+
+#%% Appends yahoo finance data
 
 
+
+
+
+#%%
 if __name__=='__main__':
     PATH_TO_SEC_DATA=os.environ['PATH_TO_SEC_DATA']
     PATH_TO_COMPUSTAT_CRSP_DATA=os.environ['PATH_TO_COMPUSTAT_CRSP_DATA']
@@ -89,10 +106,10 @@ if __name__=='__main__':
     start=time.time()
     df=pd.read_csv(os.path.join(PATH_TO_SEC_DATA, "cik_ticker.csv"))
 
-    tickers=df.ticker[2500:7500]
+    tickers=df.ticker #[1:10]
 
     #print(len(tickers))
-    get_yahoo_finance_data(tickers, 2)
+    #get_yahoo_finance_data(tickers)
     #nb= 1 #os.cpu_count() The API breaks if we try more
     # batches=np.array_split(tickers,nb)
     # print(f"Batches of size {[len(batch) for batch in batches]}")
@@ -112,3 +129,11 @@ if __name__=='__main__':
     # n=len(tickers)
     # print(f"{t} seconds - for {n} tickers")
 
+
+# #%% testing
+# ticker='tsla'
+# before = int(time.mktime(datetime.date(1990,1,1).timetuple()))# Some starting date so I dont download all
+# today=int(time.time())
+# url=f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={before}&period2={today}&interval=1d&events=history&includeAdjustedClose=true"
+     
+# %%
